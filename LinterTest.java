024e77c08ffd117b0062c993492130c09fd0c5ab
OkieMatthewHarrison <hman801@gmail.com>
2025-04-20 15:37:12 -0500

    Didn't see part about screen shots with Linter

---
+++ LinterTest.java
@@ -0,0 +1,8 @@
+public class LinterTest {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        if (true) {
+            System.out.println("This should be spaced out.");
+        }
+    }
+}
